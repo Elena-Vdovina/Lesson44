52c8a36804e0904eb155238fb365ebaf0ff31340
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:26:20 +0100

    Penguin override method

--- src/homework/Penguin.java
+++ src/homework/Penguin.java
@@ -3,4 +3,8 @@ package homework;
 // Реализуйте соответствующие интерфейсы из предыдущей задачи в классах "Человек", "Утка",
 // "Пингвин" и "Самолёт".
 public class Penguin implements CanSwim {
+
+  public void swim() {
+    System.out.println("Penguin swim");
+  }
 }
