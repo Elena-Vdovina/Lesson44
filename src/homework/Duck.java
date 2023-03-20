1da13982a3e178f61108e6c334120b015f64faaf
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:20:41 +0100

    Create class Duck

---
+++ src/homework/Duck.java
@@ -0,0 +1,6 @@
+package homework;
+
+// Реализуйте соответствующие интерфейсы из предыдущей задачи в классах "Человек",
+// "Утка", "Пингвин" и "Самолёт".
+public class Duck implements CanFly, CanRun, CanSwim{
+}
