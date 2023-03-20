0d876ddba66e051e47a28f268198a25640d12df6
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:10:14 +0100

    Create class Superman

---
+++ src/homework/Superman.java
@@ -0,0 +1,7 @@
+package homework;
+
+// Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать" CanRun
+// с методом run и "умеет плавать" CanSwim с методом swim и реализуйте их
+// в классе "Супермен" Superman.
+public class Superman implements CanFly, CanRun, CanSwim{
+}
