617b3c9f4dc5b9f0e765dc9592aeee767ddc0630
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:19:16 +0100

    Human override methods

--- src/homework/Human.java
+++ src/homework/Human.java
@@ -3,4 +3,15 @@ package homework;
 // Реализуйте соответствующие интерфейсы из предыдущей задачи в классах "Человек",
 // "Утка", "Пингвин" и "Самолёт".
 public class Human implements CanRun, CanSwim {
+
+  @Override
+  public void run() {
+    System.out.println("Human run");
+  }
+
+  @Override
+  public void swim() {
+    System.out.println("Human swim");
+  }
+
 }
