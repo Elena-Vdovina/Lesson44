ff1d14d5aa82b1be14efe1e11d162f3ec2f7fc01
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:30:10 +0100

    Plane override methods

--- src/homework/Plane.java
+++ src/homework/Plane.java
@@ -3,4 +3,14 @@ package homework;
 // Реализуйте соответствующие интерфейсы из предыдущей задачи в классах "Человек",
 // "Утка", "Пингвин" и "Самолёт".
 public class Plane implements CanFly, CanRun {
+
+  @Override
+  public void fly() {
+    System.out.println("Plane fly");
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Plane run");
+  }
 }
