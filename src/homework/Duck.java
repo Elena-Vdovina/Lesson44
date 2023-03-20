99163d26278773fb101ba112a2012ded2462ee6a
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-21 00:23:21 +0100

    Duck override methods

--- src/homework/Duck.java
+++ src/homework/Duck.java
@@ -2,5 +2,20 @@ package homework;
 
 // Реализуйте соответствующие интерфейсы из предыдущей задачи в классах "Человек",
 // "Утка", "Пингвин" и "Самолёт".
-public class Duck implements CanFly, CanRun, CanSwim{
+public class Duck implements CanFly, CanRun, CanSwim {
+
+  @Override
+  public void fly() {
+    System.out.println("Duck fly");
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Duck run");
+  }
+
+  @Override
+  public void swim() {
+    System.out.println("Duck swim");
+  }
 }
